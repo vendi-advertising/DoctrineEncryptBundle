94083611c32d36e19a3bedf143aae12a61c7161b
Chris Haas <chris@vendiadvertising.com>
2024-11-19 07:46:47 -0600

    Symfony 7.1

--- src/DependencyInjection/DoctrineEncryptExtension.php
+++ src/DependencyInjection/DoctrineEncryptExtension.php
@@ -4,7 +4,8 @@ namespace Ambta\DoctrineEncryptBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -16,22 +17,22 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class DoctrineEncryptExtension extends Extension
 {
-    const SupportedEncryptorClasses = array(
+    const SupportedEncryptorClasses = [
         'Defuse' => 'Ambta\DoctrineEncryptBundle\Encryptors\DefuseEncryptor',
         'Halite' => 'Ambta\DoctrineEncryptBundle\Encryptors\HaliteEncryptor',
-    );
+    ];
 
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         // Create configuration object
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $config        = $this->processConfiguration($configuration, $configs);
 
         // If empty encryptor class, use Halite encryptor
-        if (in_array($config['encryptor_class'], array_keys(self::SupportedEncryptorClasses))) {
+        if (array_key_exists($config['encryptor_class'], self::SupportedEncryptorClasses)) {
             $config['encryptor_class_full'] = self::SupportedEncryptorClasses[$config['encryptor_class']];
         } else {
             $config['encryptor_class_full'] = $config['encryptor_class'];
@@ -39,7 +40,7 @@ class DoctrineEncryptExtension extends Extension
 
         // Set parameters
         $container->setParameter('ambta_doctrine_encrypt.encryptor_class_name', $config['encryptor_class_full']);
-        $container->setParameter('ambta_doctrine_encrypt.secret_key_path',$config['secret_directory_path'].'/.'.$config['encryptor_class'].'.key');
+        $container->setParameter('ambta_doctrine_encrypt.secret_key_path', $config['secret_directory_path'] . '/.' . $config['encryptor_class'] . '.key');
 
         // Load service file
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
